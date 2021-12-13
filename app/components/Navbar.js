046e087df860a16076efdf28188908e7cc9f1d65
Kevin <kevin.ho@edu.devinci.fr>
2021-12-13 14:33:30 +0100

    Navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import styles from '../styles/Navbar.module.css'
+export default function Count(){
+
+  return (
+      <div className={styles.navcontainer}>
+      <p>Toto</p>
+      <p>Tata</p>
+    </div>
+  )
+}
+    
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Count from '../components/Count'
+import Navbar from '../components/Navbar'
+import styles from '../styles/App.module.css'
 
 export default function Home() {
   return (
@@ -12,22 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Count/>
+      <main>
+        <Navbar/>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
