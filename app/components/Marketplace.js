4d6d2439a5adc1359dd03a57bf07249565b85bd5
Kevin <kevin.ho@edu.devinci.fr>
2021-12-13 16:33:32 +0100

    add style and card

--- app/components/Marketplace.js
+++ app/components/Marketplace.js
@@ -8,10 +8,10 @@ export default function Marketplace () {
     return (
         <div className={styles.tittle}> <h1>Marketplace</h1>
         <div className={styles.marketcontainer}>
-            <Card tittle="CARD 1" desc="Amoug Us" price="990" image="https://monandroid.fr/wp-content/uploads/2021/01/amoung-us-933.jpg"/>
-            <Card tittle="CARD 1" desc="Amoug Us" price="990" image="https://monandroid.fr/wp-content/uploads/2021/01/amoung-us-933.jpg"/>
-            <Card tittle="CARD 1" desc="Amoug Us" price="990" image="https://monandroid.fr/wp-content/uploads/2021/01/amoung-us-933.jpg"/>
-            <Card tittle="CARD 1" desc="Amoug Us" price="990" image="https://monandroid.fr/wp-content/uploads/2021/01/amoung-us-933.jpg"/>        
+            <Card tittle="CARD 1" desc="Spiderman in the space" price="99" image="https://i.redd.it/7nk7p8gl90371.jpg"/>
+            <Card tittle="CARD 2" desc="Logo Spiderman" price="150" image="https://r1.ilikewallpaper.net/iphone-x-wallpapers/download/100006/spider-man-miles-morales-logo-4k-iphone-x-wallpaper-ilikewallpaper_com.jpg"/>
+            <Card tittle="CARD 3" desc="Animer double épées" price="300" image="http://m.gettywallpapers.com/wp-content/uploads/2020/09/Anime-Wallpaper.jpg"/>
+            <Card tittle="CARD 1" desc="Spiderman in the space" price="990" image="https://i.redd.it/7nk7p8gl90371.jpg"/>        
         </div>
     </div>    
     )
